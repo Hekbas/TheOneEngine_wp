31608bc56599c1bac6c03b5a6b867dea7f1c5511
Hekbas <34044086+Hekbas@users.noreply.github.com>
2024-01-26 05:00:00 +0100

    Updated _site html files

--- _site/assets/js/just-the-docs.js
+++ _site/assets/js/just-the-docs.js
@@ -53,7 +53,7 @@ function initNav() {
 
 function initSearch() {
   var request = new XMLHttpRequest();
-  request.open('GET', '/Easings_and_Splines_Research/assets/js/search-data.json', true);
+  request.open('GET', '/TheOneEngine_wp/assets/js/search-data.json', true);
 
   request.onload = function(){
     if (request.status >= 200 && request.status < 400) {
@@ -432,7 +432,7 @@ jtd.getTheme = function() {
 
 jtd.setTheme = function(theme) {
   var cssFile = document.querySelector('[rel="stylesheet"]');
-  cssFile.setAttribute('href', '/Easings_and_Splines_Research/assets/css/just-the-docs-' + theme + '.css');
+  cssFile.setAttribute('href', '/TheOneEngine_wp/assets/css/just-the-docs-' + theme + '.css');
 }
 
 // Scroll site-nav to ensure the link to the current page is visible
